e9cd29ea698774aeee43134bb00b0e51b6fbde56
joelatdeluxe <joelsmith.developer@gmail.com>
2023-11-20 23:20:57 -0800

    refactor part 1

---
+++ src/main/scala/day10/emu.scala
@@ -0,0 +1,37 @@
+package day10
+
+import scala.collection.mutable.Queue
+
+class Ticker(val source: Iterator[String]) extends Iterator[Int] {
+  var cellValue = 1
+  val actionQueue: Queue[Action] = Queue()
+
+  def hasNext: Boolean = {
+    actionQueue.size > 0 || source.hasNext
+  }
+
+  def next(): Int = {
+    if (actionQueue.isEmpty) { // pull the next element
+      if (!source.hasNext) {
+        throw new Exception("Stop iterating!")
+      }
+      val rawEvent = source.next()
+      val action = rawEvent match {
+        case "noop" => Noop()
+        case line => AddX(line.split(" ", 2)(1).toInt) 
+      }
+      // ignore tick timer -- just add on empty steps
+      val bufferActions = (0 until action.ticks - 1)
+        .map(_=>Noop()) :+ action
+
+      actionQueue.enqueue(bufferActions:_*)
+    }
+    val step = actionQueue.dequeue()
+    val cellInc = step match {
+        case AddX(v) => v
+        case Noop() => 0
+    }
+    cellValue = cellValue + cellInc
+    cellValue
+  }
+}
